a3f3f7429f9d560b892086dc0c4496dde5267e73
gracehsu-lin <78342010+gracehsu-lin@users.noreply.github.com>
2023-01-30 12:42:56 +0800

    Create ex1-3.cpp

---
+++ ex1-3.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main (void) {
+     printf(" this building is %10d cm \n", 890);
+     printf(" this building is %10d cm \n", 7890);
+     printf(" this building is %10d cm \n", 67890);
+     printf(" this building is %10d cm \n", 567890);
+     printf(" this building is %10d cm \n", 1234567890);
+      printf(" pi = %22f \n ", 3.1415926535897932384626433832795);
+}
