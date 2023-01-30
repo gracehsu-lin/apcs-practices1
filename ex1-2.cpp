4bd8ac992464893e55ddeedf5c822ada7bb474bf
gracehsu-lin <78342010+gracehsu-lin@users.noreply.github.com>
2023-01-30 10:55:34 +0800

    Create ex1-2.cpp

---
+++ ex1-2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello World, Grace, your height is " << 1 >> " m " << 70 << " cm" >> end1 ;
+    printf("Hello World, Grace, your height is %d m %d cm \n"1,70);
+    return 0;
+}
